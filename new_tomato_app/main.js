2d843698ddbe1ee0282e34ea8ddb3fba3cc5a9d9
noahtj <noahtj@bgsu.edu>
2022-01-18 20:16:28 -0500

    commit

--- new_tomato_app/main.js
+++ new_tomato_app/main.js
@@ -4,7 +4,7 @@ import './style.css'
 function getCountDownDate() {
   // TODO: Build countdown date dynamically 
 
-  var countDownDate = new Date("Jan 18, 2022 20:06:25").getTime();
+  var countDownDate = new Date("Jan 18, 2022 20:10:25").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
@@ -16,14 +16,11 @@ function getCountDownDate() {
     var distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
-    document.getElementById("timer-display").innerHTML = days + "d " + hours + "h "
-      + minutes + "m " + seconds + "s ";
+    document.getElementById("timer-display").innerHTML = minutes + ":" + seconds;
 
     // If the count down is over, write some text 
     if (distance < 0) {
