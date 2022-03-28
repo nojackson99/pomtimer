fbc7fdc902917450ecf61ae6fc324bfddc3eee85
noahtj <noahtj@bgsu.edu>
2022-03-28 19:04:15 -0400

    add active task below start focus button html and css

--- new_tomato_app/js/main.js
+++ new_tomato_app/js/main.js
@@ -1,5 +1,15 @@
 // [X] todo: break away as much code from main into modules
 
+// HTML todo's
+// [] todo: show active task below start focus button
+// [] todo: settings button that opens modal (maybe in top right?)
+// [] todo: profie button that allows for switching to new profile or creating a new one (maybe top left)
+// [] todo: checkbox next to each task that when clicked strikesthrough task
+// [] todo:
+// [] todo:
+// [] todo:
+// [] todo:
+
 
 // -------------- IMPORTS ---------------------
 import '../style.css';
