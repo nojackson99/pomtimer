9c6f3624e198ce1df135f33e521d0c7ad47b2416
noahtj <noahtj@bgsu.edu>
2022-03-31 08:51:38 -0400

    commit

--- new_tomato_app/js/main.js
+++ new_tomato_app/js/main.js
@@ -21,8 +21,3 @@ import * as Header from './header.js';    //header associated code
 import * as Tasks from './tasks.js';      //task associated code
 import * as Theme from './theme.js';      //theme associated code
 import * as MyTimer from './timer.js';    //timer associated code
-
-
-
-
-
