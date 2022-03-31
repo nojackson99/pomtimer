61d2420045b946f840d991fab66e02f66f849591
noahtj <noahtj@bgsu.edu>
2022-03-30 20:09:06 -0400

    refactor header.js and improve comments

--- new_tomato_app/js/header.js
+++ new_tomato_app/js/header.js
@@ -1,31 +1,66 @@
-import * as Theme from './theme.js'
-import * as MyTimer from './timer'
+//================================================----header.js----================================================
+// The code in this file controls the profile and settings buttons at the top of the page.
+// Hoevering over profile menu opens the dropdown which allows for switching profiles and creating a new profile.
+// Clicking create new profile opens profile modal which contains a form to submit new profile data.
+// Clicking settings opens settings modal which contains a form to submit settings changes
+//================================================-----------------================================================
 
-export const newProfileButton = document.querySelector("#create-new-profile")
-export const profileModal = document.querySelector("#new-profile-modal")
-export const closeProfileModal = document.querySelector("#close-profile-modal")
+// -------------- IMPORTS ---------------------
+import * as Theme from './theme.js' // theme associated code
+
+
+// -------------- PROFILE ---------------------
+// profile html elements
+export const newProfileButton = document.querySelector("#create-new-profile");
+export const profileModal = document.querySelector("#new-profile-modal");
+export const closeProfileModal = document.querySelector("#close-profile-modal");
+
+// profile dropdown list
+export const dropdownContent = document.querySelector("#dropdown-content");
+
+// new profile form elements
 export const newProfileFirstName = document.querySelector("#new-profile-first-name")
 export const newProfileLastName = document.querySelector("#new-profile-last-name");
 export const newProfileUsername = document.querySelector("#new-profile-username");
 export const newProfileSubmit = document.querySelector("#new-profile-submit");
-const settingsButton = document.querySelector("#settings-button")
-const settingsModal = document.querySelector("#settings-modal")
-const closeSettingsModal = document.querySelector("#close-settings-modal") 
-const settingsSubmit = document.querySelector("#settings-submit");
 
-const pomodoroLength = document.querySelector("#pomodoro-length")
-const shortBreak = document.querySelector("#settings-short-break")
-const longBreak = document.querySelector("#settings-long-break");
-
+// open new profile modal
 newProfileButton.addEventListener('click', ()=> {
     profileModal.showModal();
 })
 
+// close new profile modal
 closeProfileModal.addEventListener('click', ()=> {
     profileModal.close();
 })
 
+// add new profile to profilesArray
+newProfileSubmit.addEventListener('click', ()=> {
+    // get values from new profile form
+    const fName = newProfileFirstName.value;
+    const lName = newProfileLastName.value;
+    const uName = newProfileUsername.value;
+
+    // call funciton to create new profile
+    Data.newProfileSubmit(fName,lName,uName);
+});
+
+
+// -------------- SETTINGS --------------------
+// settings html elements
+const settingsButton = document.querySelector("#settings-button");
+const settingsModal = document.querySelector("#settings-modal");
+const closeSettingsModal = document.querySelector("#close-settings-modal") ;
+
+// session interval form elements
+const pomodoroLength = document.querySelector("#pomodoro-length");
+const shortBreak = document.querySelector("#settings-short-break");
+const longBreak = document.querySelector("#settings-long-break");
+const settingsSubmit = document.querySelector("#settings-submit");
+
+// open settings profile modal
 settingsButton.addEventListener('click', ()=> {
+    // populate form values with current session lengths
     pomodoroLength.value = Theme.workLength;
     shortBreak.value = Theme.shortBreak;
     longBreak.value = Theme.longBreak;
@@ -33,27 +68,18 @@ settingsButton.addEventListener('click', ()=> {
     settingsModal.showModal();
 })
 
+// close settings profile modal
 closeSettingsModal.addEventListener('click', ()=> {
     settingsModal.close();
 })
 
+// confirm settings changes
 settingsSubmit.addEventListener('click', ()=> {
     const pom = pomodoroLength.value;
     const short = shortBreak.value;
     const long = longBreak.value;
 
+    // call function to update session lengths
     Theme.updateSessionLengths(pom,short,long);
-
-
 })
-
-// add new profile to profilesArray
-newProfileSubmit.addEventListener('click', ()=> {
-    const fName = Header.newProfileFirstName.value;
-    const lName = Header.newProfileLastName.value;
-    const uName = Header.newProfileUsername.value;
-    Data.newProfileSubmit(fName,lName,uName)
-});
-
-export const dropdownContent = document.querySelector("#dropdown-content");
 
